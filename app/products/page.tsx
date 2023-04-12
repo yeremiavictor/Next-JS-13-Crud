e644c5464c468baa9a7d06840ecd2fc6ff5f1a85
Yeremia Victor <yeremiavictor@gmail.com>
2023-04-12 15:56:26 +0700

    init page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,41 @@
-export default function ProductList() {
+type Product = {
+    id:number;
+    title:string;
+    price:number;
+}
+
+async function getProducts(){
+    // ini adalah static (perlu refresh untuk update data)
+    const res = await fetch('http://localhost:5000/products')
+    return res.json()
+}
+
+export default async function ProductList() {
+    const products: Product[] = await getProducts();
   return (
-    <div>ProductList</div>
+    <div className="py-10 px-10">
+        <table className="table w-full">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Product Name</th>
+                    <th>price</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {products.map((product, index) =>(
+                    <tr key={product.id}>
+                        <td>{index +1}</td>
+                        <td>{product.title}</td>
+                        <td>{product.price}</td>
+                    </tr>
+                ))}
+
+            </tbody>
+            
+        </table>
+    </div>
   )
 }
